79c397c2f96f5b648f2cb34b7f1a756d20f8d3d3
013 <tswizzle@live.co.uk>
2014-02-17 15:17:25 +0100

    Timed based movement

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
 var score = 0.0;
 var currentLetter = "";
 var words = [];
-getWords();
+getWords(); // Populates 'words'
 // Get first word
 var wordToType = words[0]
 var letterPlace = 0;
 var wordPlace = 0;
-var letterToType = wordToType[letterPlace];
+var letterToType = wordToType[letterPlace]; //words[0][0]
+var angle = Math.floor(Math.random()*(61 - -60 +1)+ -61);
+var z=0;
+var canvas = document.getElementById("moving");
+var context = canvas.getContext("2d");
 
-/* Function relating to canvas */
-function redraw(wordToType, letterPlace, correct) {
-	var canvas = document.getElementById("moving");
-	var context = canvas.getContext("2d");
-	var x = 60;
-	var y = 15;
-	canvas.width = 120;
-	canvas.height = 20;
+var correct;
+
+var now, delta;
+var then = new Date().getTime();
+
+window.requestAnim = function() {
+	return window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		function(f) {
+			window.setTimeout(f,1e3/60);
+		}
+}();
+
+function move() {
+	now = new Date().getTime();
+	delta = now-then;
+	//console.log(delta);
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	var x = 0//Math.floor(Math.random()*(50 - -50 +1)+ -50);
+	var y = 0;
 	
-	context.font = "12px monospace";
-	context.textAlign = "center";
-
+	context.font = "24px monospace";
+	z+=calcSpeed(delta, 1);
+	context.translate(canvas.width / 2, 20+z);
+	if (angle > -80) {
+		angle -= 2;
+	}
+	context.rotate(angle*Math.PI / 180);
+	context.save();
 	for (var i=0; i<=wordToType.length; ++i) {
 		var ch = wordToType.charAt(i);
 		if (i<letterPlace) {	
@@ -32,9 +57,24 @@ function redraw(wordToType, letterPlace, correct) {
 		context.fillText(ch, x, y);
 		x += context.measureText(ch).width;
 	}
+	context.restore();
+
+	then = now;
+
 }
 
+var calcSpeed = function(delta, speed) {
+	return(speed * 60 * delta) / 1000;
+}
+
+var anim = function() {
+	requestAnim(anim);
+	move();
+}
+anim();
+
 function nextword(word) {
+	return 0;
 	var canvas = document.getElementById("nextword");
 	var context = canvas.getContext("2d");
 	var x = 60;
@@ -48,91 +88,59 @@ function nextword(word) {
 	context.fillText(word, x, y);
 }
 
-redraw(wordToType, letterPlace, true);
 nextword(words[1]);
-/*				*/
 
 document.addEventListener('keydown', function(event) {
 	// Add to the var 'currentWord' the current pressed key
 	currentLetter = String.fromCharCode(event.keyCode);
-
+	
 	if(event.keyCode == 32) { // Space
 		console.log("PAUSE");
 	}
 
-	if (letterToType.toUpperCase() == currentLetter) {
-		//console.log(letterToType);
-		letterPlace += 1;
-		letterToType = wordToType[letterPlace];
-		
-		// Make colours
-		//	document.getElementById('colourword').innerHTML = 
-		//	//"<span style='color: #00DD00'>" + wordToType.substr(0,letterPlace) + "<span>" + 
-		//	"<span style='color: #CCC'>" + wordToType.substr(0,letterPlace) + "</span>" + 
-		//	"<span style='color: #000'>" + wordToType.substr(letterPlace, wordToType.length) + "</span>";
-		//
-		redraw(wordToType, letterPlace, true);
-		
-		// Word completely typed
-		if (wordToType.length == letterPlace) {
-			score += 1;
-			document.getElementById('score').innerHTML = score;
-			// Reset some variables
-			wordPlace += 1;
-			
-			wordToType = words[wordPlace];
-			//document.getElementById('word').innerHTML = wordToType;
-			//document.getElementById('colourword').innerHTML = "<span style='color: #000'>" + wordToType + "</span>";
-			
-			// if need new words, get them
-			if (wordPlace == words.length-1) {
-				//console.log("Need new words");
-				getWords();
-				wordPlace = -1;
-			}
-			
-			redraw(wordToType);
-			nextword(words[wordPlace+1]);
-			//document.getElementById('nextword').innerHTML = words[wordPlace+1];
-			
-			letterPlace = 0;
+	// A - Z	
+	if (event.keyCode >= 65 && event.keyCode <= 90) {
+		if (letterToType.toUpperCase() == currentLetter) {
+			//console.log(letterToType);
+			letterPlace += 1;
 			letterToType = wordToType[letterPlace];
+			
+			correct = true;
+			
+			// Word completely typed
+			if (wordToType.length == letterPlace) {
+				score += 1;
+				angle = 0;
+				z = 0;
+				//document.getElementById('score').innerHTML = score;
+				// Reset some variables
+				wordPlace += 1;
+				
+				wordToType = words[wordPlace];
+				
+				// if need new words, get them
+				if (wordPlace == words.length-1) {
+					getWords();
+					wordPlace = -1;
+				}
+				
+				nextword(words[wordPlace+1]);
+				
+				letterPlace = 0;
+				letterToType = wordToType[letterPlace];
+			}
+		} else {
+			// Typed incorrect letter
+			score -= 0.1;
+			// Since 0.1 isn't actually 0.1, we need to round it
+			score = Math.round(score * 100) / 100;
+			//document.getElementById('score').innerHTML = score;
+			correct = false;
+			//redraw(wordToType, letterPlace, false);
 		}
-	} else {
-		// Typed incorrect letter
-		score -= 0.1;
-		// Since 0.1 isn't actually 0.1, we need to round it
-		score = Math.round(score * 100) / 100;
-		document.getElementById('score').innerHTML = score;
-
-		redraw(wordToType, letterPlace, false);
-		/* Fade colour */
-
-		//document.getElementById('colourword').innerHTML = 
-		//"<span style='color: #00DD00'>" + wordToType.substr(0,letterPlace) + "<span>" + 
-		//"<span style='color: #CCC'>" + wordToType.substr(0,letterPlace) + "</span>" + 
-		//"<span id='flash' style='transition: color 0.2s; -moz-transition: color 0.2s; -webkit-transition: color 0.2s;'>" + wordToType.charAt(letterPlace) + "</span>" + 
-		//"<span style='color: #000'>" + wordToType.substr(letterPlace+1, wordToType.length) + "</span>";
-		//var flash = document.getElementById('flash');
-		//flash.style.transition = "color 0.5s ease-in 0.5s";
-		//flash.style.WebkitTransition = 'color 0.5s ease-in 0.5s';
-		//flash.style.MozTransition = 'color 0.5s ease-in 0.5s';
-		//setTimeout(function() {
-		//	var flash = document.getElementById('flash');
-		//	flash.style.color = "#dd0000";
-		//},0);
-		/*		*/
-
 	}
 });
-/*
-function fade() {
-		document.getElementById('flash').style.transition = "color 0.5s ease-in 0.5s";
-		document.getElementById('flash').style.WebkitTransition = 'color 0.5s ease-in 0.5s';
-		document.getElementById('flash').style.MozTransition = 'color 0.5s ease-in 0.5s';
-		document.getElementById('flash').style.color = "#dd0000";
-}
-*/
+
 function getWords() {
 	request = new XMLHttpRequest;
 	// Difficulty starts at 2
